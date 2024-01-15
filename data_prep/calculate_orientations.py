7d49a597c666ee4810fa390b28d4b6c23fec7a5a
Dave Whipps <dwhipps@simulationcurriculum.com>
2024-01-15 15:48:25 -0500

    Calculates some statistics of pitch and roll angles.

--- data_prep/calculate_orientations.py
+++ data_prep/calculate_orientations.py
@@ -23,6 +23,8 @@ df['roll'] = df.apply( calcRoll, axis=1 )
 print( 'Min/Max pitch angle:', df.min(axis=0)['pitch'], df.max(axis=0)['pitch'])
 print( 'Min/Max roll angle:', df.min(axis=0)['roll'], df.max(axis=0)['roll'])
 
+banked_left_only = df.loc[df['label'] == 'banked-left']
+banked_right_only = df.loc[df['label'] == 'banked-right']
 grass_only = df.loc[df['label'] == 'grass']
 flat_even_only = df.loc[df['label'] == 'flat-even']
 sloped_down_only = df.loc[df['label'] == 'sloped-down']
@@ -32,3 +34,28 @@ print( 'Min/Max grass pitch angle:', grass_only.min(axis=0)['pitch'], grass_only
 print( 'Min/Max grass flat_even angle:', flat_even_only.min(axis=0)['pitch'], flat_even_only.max(axis=0)['pitch'])
 print( 'Min/Max sloped-down pitch angle:', sloped_down_only.min(axis=0)['pitch'], sloped_down_only.max(axis=0)['pitch'])
 print( 'Min/Max sloped-up pitch angle:', sloped_up_only.min(axis=0)['pitch'], sloped_up_only.max(axis=0)['pitch'])
+
+#Get all of the labels
+labels = df['label'].unique()
+
+# Get stats for the pitch
+pitch_stats = pd.DataFrame(columns=['min', 'max', 'mean', 'std'])
+for label in labels:
+	labels_rows = df.loc[df['label'] == label]['pitch']
+	pitch_stats.at[label, 'min']  = labels_rows.min(axis=0)
+	pitch_stats.at[label, 'max']  = labels_rows.max(axis=0)
+	pitch_stats.at[label, 'mean'] = labels_rows.mean(axis=0)
+	pitch_stats.at[label, 'std']  = labels_rows.std(axis=0)
+
+print(pitch_stats)
+
+# Get stats for the roll
+roll_stats = pd.DataFrame(columns=['min', 'max', 'mean', 'std'])
+for label in labels:
+	labels_rows = df.loc[df['label'] == label]['roll']
+	roll_stats.at[label, 'min']  = labels_rows.min(axis=0)
+	roll_stats.at[label, 'max']  = labels_rows.max(axis=0)
+	roll_stats.at[label, 'mean'] = labels_rows.mean(axis=0)
+	roll_stats.at[label, 'std']  = labels_rows.std(axis=0)
+
+print(roll_stats)
